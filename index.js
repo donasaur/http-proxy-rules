7eb809b14d0aad7d7e2e56f57ea95e91f5577f30
donasaur <donhmai@gmail.com>
2015-11-13 03:00:28 -0800

    Added http proxy table code

---
+++ index.js
@@ -0,0 +1,49 @@
+
+function HttpProxyTable(options) {
+  this.rules = options.rules;
+  this.default = options.default || null;
+
+  return this;
+};
+
+/**
+ * This function will modify the `req` object if a match is found.
+ * We also return the new endpoint string if a match is found.
+ * @param  {Object} options Takes in a `req`, `rules`, (optional) `default` params.
+ */
+HttpProxyTable.prototype.test = function test(req) {
+  var rules = this.rules;
+  var target = this.default;
+  var path = req.url;
+
+  // go through the proxy table, assuming keys (path prefixes) are ordered
+  // and pick the first target whose path prefix is a prefix of the
+  // request url path. RegExp enabled.
+  var pathPrefixRe;
+  var testPrefixMatch;
+  var urlPrefix;
+  var pathEndsWithSlash;
+  for (var pathPrefix in rules) {
+    if (rules.hasOwnProperty(pathPrefix)) {
+      if (pathPrefix[pathPrefix.length - 1] === '/') {
+        pathPrefixRe = new RegExp(pathPrefix);
+        pathEndsWithSlash = true;
+      } else {
+        // match '/test' or '/test/' or './test?' but not '/testing'
+        pathPrefixRe = new RegExp('(' + pathPrefix + ')' + '(?:\\W|$)');
+        pathEndsWithSlash = false;
+      }
+      testPrefixMatch = pathPrefixRe.exec(path);
+      if (testPrefixMatch && testPrefixMatch.index === 0) {
+        urlPrefix = pathEndsWithSlash ? testPrefixMatch[0] : testPrefixMatch[1];
+        req.url = path.replace(urlPrefix, '');
+        target = rules[pathPrefix];
+        break;
+      }
+    }
+  }
+
+  return target;
+}
+
+module.exports = HttpProxyTable;
